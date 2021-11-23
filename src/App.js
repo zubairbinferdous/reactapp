ed8678bbeacba4ae14add416700042f48f97ac3f
zubairbinferdous <zubairbinf@gmail.com>
2021-11-23 13:08:32 +0600

    ok

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> love me more</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque facilis eos voluptatum quas praesentium repellat ipsa, obcaecati quo dolorem maxime qui, in earum, enim est culpa! Repellat totam temporibus quaerat, culpa maiores itaque dicta libero veritatis similique iusto, tempora illo modi perferendis perspiciatis     consectetur doloribus, voluptatum id deleniti iste velit porro fugiat laboriosam. Voluptate nam officia unde hic eum deserunt non voluptatibus dolorum labore nesciunt fugiat corrupti ipsa, eos error pariatur quia aperiam temporibus eaque, cupiditate veritatis amet molestias earum odio. Debitis, omnis velit! Nam molestiae nemo consequatur rem odit fugiat consectetur at eaque voluptatem rerum a, recusandae eius officiis.</p>
+
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque facilis eos voluptatum quas praesentium repellat ipsa, obcaecati quo dolorem maxime qui, in earum, enim est culpa! Repellat totam temporibus quaerat, culpa maiores itaque dicta libero veritatis similique iusto, tempora illo modi perferendis perspiciatis     consectetur doloribus, voluptatum id deleniti iste velit porro fugiat laboriosam. Voluptate nam officia unde hic eum deserunt non voluptatibus dolorum labore nesciunt fugiat corrupti ipsa, eos error pariatur quia aperiam temporibus eaque, cupiditate veritatis amet molestias earum odio. Debitis, omnis velit! Nam molestiae nemo consequatur rem odit fugiat consectetur at eaque voluptatem rerum a, recusandae eius officiis.</p>
+      <div className="name-part">
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure illum aliquam quidem officia tempora labore, voluptatem odit esse expedita itaque nisi voluptatibus sed magni minima quos atque quasi aliquid voluptas, distinctio assumenda! Eum voluptates, debitis nisi aperiam quas voluptatem a nemo excepturi incidunt, animi inventore officia, magni illo eligendi saepe.</p>
+      </div>
     </div>
   );
 }
